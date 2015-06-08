87a0a428521e54f9d86ec84faa7d9ac71093f155
rubtest <cs-gmf@gmx.de>
2015-06-08 18:11:58 +0200

    Update original.php

--- original.php
+++ original.php
@@ -1,6 +1,25 @@
-<?php echo $title; ?></title>
-    <?php
+<?php 
 
-    wp_admin_css( 'login', true );
+class App7 {
+	var $url;
+	public function setUrl($url) {
+		$this->url = $url;
+	}
 	
-	?>
+	public function printUrl() {
+		if(substr($this->url, 1, 4) == 'http') {
+			echo $this->url;
+		}
+	}
+	
+}
+
+$passedUrl = $_GET['url'];
+$ds1 = new App7;
+$ds1->setURL($passedUrl);
+$ds1->url = 1;
+$ds1->printUrl();
+
+$ds2 = new App7;
+$ds2->setURL($passedUrl);
+$ds2->printUrl();
